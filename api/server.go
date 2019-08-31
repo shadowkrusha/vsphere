613fca5a646c6049526158d78ad047f825e20494
Iain Riley <iain.riley@westernpower.com.au>
2019-08-31 19:43:17 +0800

    update server

--- api/server.go
+++ api/server.go
@@ -3,6 +3,7 @@ package api
 import (
 	"fmt"
 	log "github.com/Sirupsen/logrus"
+	"github.com/gorilla/mux"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	unrender "github.com/unrolled/render"
 	"net/http"
@@ -25,18 +26,20 @@ func (s *HttpServer) Start() {
 		Layout:     "layout",
 	})
 
-	http.Handle("/metrics", promhttp.Handler())
+	r := mux.NewRouter()
 
-	http.HandleFunc("/ping", func(w http.ResponseWriter, req *http.Request) {
+	r.Handle("/metrics", promhttp.Handler())
+
+	r.HandleFunc("/ping", func(w http.ResponseWriter, req *http.Request) {
 		render.Text(w, http.StatusOK, "pong")
 	})
-	http.HandleFunc("/config", func(w http.ResponseWriter, req *http.Request) {
+	r.HandleFunc("/config", func(w http.ResponseWriter, req *http.Request) {
 		render.JSON(w, http.StatusOK, s.Config)
 	})
-	http.HandleFunc("/status", func(w http.ResponseWriter, req *http.Request) {
+	r.HandleFunc("/status", func(w http.ResponseWriter, req *http.Request) {
 		render.Text(w, http.StatusOK, "OK")
 	})
-	http.HandleFunc("/version", func(w http.ResponseWriter, req *http.Request) {
+	r.HandleFunc("/version", func(w http.ResponseWriter, req *http.Request) {
 		info := map[string]string{
 			// "syros_version": version,
 			"os":         runtime.GOOS,
@@ -48,12 +51,15 @@ func (s *HttpServer) Start() {
 		}
 		render.JSON(w, http.StatusOK, info)
 	})
-	http.HandleFunc("/data/pvc", func(w http.ResponseWriter, req *http.Request) {
+	r.HandleFunc("/data/pvc/{subject}", func(w http.ResponseWriter, req *http.Request) {
 		c, err := NewVSphereCollector("https://svc_datacollector:OinlMqkyfSxfuC67HGa0@pvc.ads.westernpower.com.au/sdk")
 		if err != nil {
 			fmt.Println("Error", err)
 		}
 
+		vars := mux.Vars(req)
+		subject := vars["subject"]
+
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 
 		data, err := c.Collect()
@@ -63,17 +69,27 @@ func (s *HttpServer) Start() {
 		}
 
 		results := &Results{}
-		results.Data = data.VMs
+		switch subject {
+		case "vms":
+			results.Data = data.VMs
+		case "hosts":
+			results.Data = data.Hosts
+		case "datastores":
+			results.Data = data.DataStores
+		}
 
 		render.JSON(w, http.StatusOK, results)
 	})
 
-	http.HandleFunc("/data/svc", func(w http.ResponseWriter, req *http.Request) {
+	r.HandleFunc("/data/svc/{subject}", func(w http.ResponseWriter, req *http.Request) {
 		c, err := NewVSphereCollector("https://svc_datacollector:OinlMqkyfSxfuC67HGa0@svc.ads.westernpower.com.au/sdk")
 		if err != nil {
 			fmt.Println("Error", err)
 		}
 
+		vars := mux.Vars(req)
+		subject := vars["subject"]
+
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 
 		data, err := c.Collect()
@@ -83,10 +99,23 @@ func (s *HttpServer) Start() {
 		}
 
 		results := &Results{}
-		results.Data = data.VMs
+		switch subject {
+		case "vms":
+			results.Data = data.VMs
+		case "hosts":
+			results.Data = data.Hosts
+		case "datastores":
+			results.Data = data.DataStores
+		}
 
 		render.JSON(w, http.StatusOK, results)
 	})
 
-	log.Error(http.ListenAndServe(fmt.Sprintf(":%v", s.Config.Port), http.DefaultServeMux))
+	srv := &http.Server{
+		Handler: r,
+		Addr:    fmt.Sprintf(":%v", s.Config.Port),
+	}
+
+	// log.Error(http.ListenAndServe(fmt.Sprintf(":%v", s.Config.Port), http.DefaultServeMux))
+	log.Error(srv.ListenAndServe())
 }
